4bf8986b9291de4571e45923b8b53d5973e2a34b
Nikita Malykhin <43642381+Truficop@users.noreply.github.com>
2024-06-05 10:25:34 +0000

    Generador de nombres de dominio hecho

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extencions = [".com", ".net", ".us", ".io"];
+  let allResults = [];
+
+  for (let p of pronoun) {
+    for (let a of adj) {
+      for (let n of noun) {
+        for (let e of extencions) {
+          let result = p + a + n + e;
+          allResults.push(result);
+        }
+      }
+    }
+  }
+  console.log(allResults);
+  return allResults;
 };
